54e2c9b5e6c1e1b1529492fb91481754e86a8402
Тереза Опанска <TNOpanska19@codingburgas.bg>
2022-03-16 17:01:57 +0200

    Create namespaces

--- Project/ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ Project/ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -4,11 +4,24 @@
 
 using namespace std;
 
+namespace bg
+{
+    namespace burgas
+    {
+        struct targovska {};
+
+        namespace mr
+        {
+            struct pgkpi {};
+        }
+    }
+}
+
 double* foo()
 {
     double* arr = new double(10);
 
-    delete[] arr;
+    // delete[] arr;
 
     return arr;
 }
@@ -23,5 +36,11 @@ struct USER
 
 int main()
 {
-    //
+    vector<USER> users
+    {
+        {1, "user1", "user@gmail.com", 16},
+        {1, "user1", "user@gmail.com", 17},
+        {1, "user1", "user@gmail.com", 18},
+        {1, "user1", "user@gmail.com", 19}
+    };
 }
